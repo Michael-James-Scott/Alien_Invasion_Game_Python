645402024f182db8ab4f1da761a10c852a9aa096
Mike Scott <mikescott9909@gmail.com>
2020-02-13 17:35:37 -0600

    added images

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame as pg
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -10,8 +11,13 @@ class AlienInvasion:
         self.settings = Settings()
         self.screen = pg.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pg.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
+
         # Set the background color.
         self.bg_color = (230, 230, 230)
+
+
     def run_game(self):
         """Start the main loop for the game."""
         while True:
@@ -21,10 +27,12 @@ class AlienInvasion:
                     sys.exit()
             # Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
+
+            self.ship.blitme()
+
             # Make the most recently drawn screen visible.
             pg.display.flip()
-            # Make the most recently drawn screen visible.
-            pg.display.flip()
+            
 if __name__ == '__main__':
     # Make a game instance, and run the game.
     ai = AlienInvasion()
